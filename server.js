76126c90d1cce1a66ea57207e883b42663245578
Ryan Hubble <rhubble1987@gmail.com>
2021-01-24 13:39:48 -0500

    updated  server file

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express');
 
+const expressHandlebars = require('express-handlebars');
+
 const app = express();
 
 const PORT = process.env.PORT || 8088;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log(`App now listening at localhost ${PORT}.`);
+});
 
